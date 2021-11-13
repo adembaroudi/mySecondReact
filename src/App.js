85935ed312455cf18be9180f338d26d78176693e
Adem Baroudi <adembaroudi3177@gmail.com>
2021-11-13 17:50:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card } from 'react-bootstrap';
+import gmc from './gmc.png'
+import'./style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+        <Card border="info" style={{ width: '25rem' }}>
+    <Card.Header><h1 className="title red">Adem Baroudi</h1></Card.Header>
+    <Card.Body>
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+<br/>
+<img src={gmc} style={{width:'400px', height:'200px'}} />
+<br/>
+<img src="./go-my-code.png" style={{width:'400px', height:'200px'}} />
+</div>
+<iframe style={{width:'400px', height:'240'}} src="https://www.youtube.com/embed/T7gYwl9sTQY?list=RDT7gYwl9sTQY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
     </div>
   );
 }
